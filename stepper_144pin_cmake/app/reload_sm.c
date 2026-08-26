72f85b15c0001f37a93b27d6b1718ea9bf0bd7da
Chris Owens <cowens@eemn.io>
2026-08-25 20:43:22 -0500

    feat(reload): introduce reload_sm_t module with result pattern
    - Create reload_sm.h with reload_sm_t, reload_result_t,
      reload_status_enum types and function declarations
    - Change reload_sm_start() to take reload_sm_t* and cartridge array
    - Change reload_sm_dispatch() to return reload_result_t instead of
      mutating sm->reload directly
    - Add reload_sm_abort() for explicit abort support
    - Replace include of autoloader_sm.h with reload_sm.h

--- stepper_144pin_cmake/app/reload_sm.c
+++ stepper_144pin_cmake/app/reload_sm.c
@@ -5,14 +5,14 @@
  * @version 0.1
  * @date 2026-08-07
  *
- * @copyright Copyright (c) 2026 Embedded Design Solutions, LLC. All Rights Reserved.
+ * @copyright Copyright (c) 2026 Embedded Design Solutions, LLC.  All Rights Reserved.
  */
 
 /*******************************************************************************
  * Includes
  *******************************************************************************/
 
-#include "autoloader_sm.h"
+#include "reload_sm.h"
 
 #include <stddef.h>
 
@@ -38,98 +38,115 @@
  * Public Function Definitions
  *******************************************************************************/
 
-void reload_sm_start(app_sm_t* sm) {
+void reload_sm_start(reload_sm_t* sm, cartridge_t cartridges[APP_SLOT_COUNT]) {
   if (sm != NULL) {
-    sm->reload = RELOAD_HOME_PUSHERS;
-    for (uint8_t slot = 0U; slot < APP_SLOT_COUNT; slot++) {
-      sm->cartridge[slot].pusher_homed = false;
-      sm->cartridge[slot].lifter_homed_down = false;
-      sm->cartridge[slot].lifter_homed_up = false;
-      // app_sm_port_home_pusher(slot);
+    sm->state = RELOAD_HOME_PUSHERS;
+    sm->lift_homing_end_time_ms = 0U;
+    sm->lift_homing_timeout_ms = 0U;
+
+    for (uint8_t i = 0U; i < APP_SLOT_COUNT; i++) {
+      app_sm_port_home_pusher(&cartridges[i]);
     }
-    app_sm_port_arm_timeout(APP_SM_TIMEOUT_MOTION, 1000);
+    app_sm_port_arm_timeout(APP_SM_TIMEOUT_MOTION, 10000);
   }
 }
 
-void reload_sm_dispatch(app_sm_t* sm, const app_event_t* event) {
-  if ((sm == NULL) || (event == NULL)) {
-    return;
-  }
+reload_result_t reload_sm_dispatch(reload_sm_t* reload, cartridge_t cartridges[APP_SLOT_COUNT], const app_event_t* event) {
+  reload_result_t result = {
+      .status = RELOAD_STATUS_IN_PROGRESS,
+      .fault_code = 0U,
+  };
 
-  if (event->id == APP_EV_TIMEOUT) {
-    sm->reload = RELOAD_FAILED;
-    return;
+  if ((reload == NULL) || (cartridges == NULL) || (event == NULL)) {
+    result.status = RELOAD_STATUS_FAILED;
   }
-
-  switch (sm->reload) {
-    case RELOAD_HOME_PUSHERS:
-      if (event->id == APP_EV_MOTION_DONE) {
-        app_sm_port_cancel_timeout();
-        sm->reload = RELOAD_HOME_LIFTS;
-        for (uint8_t slot = 0U; slot < APP_SLOT_COUNT; slot++) {
-          // app_sm_port_home_lift(slot, DIR_LIFTER_UP);
+  else if (event->id == APP_EV_TIMEOUT) {
+    reload->state = RELOAD_FAILED;
+    result.status = RELOAD_STATUS_FAILED;
+  }
+  else {
+    switch (reload->state) {
+      case RELOAD_HOME_PUSHERS:
+        if (event->id == APP_EV_MOTION_DONE) {
+          app_sm_port_cancel_timeout();
+          reload->state = RELOAD_HOME_LIFTS;
+          for (uint8_t slot = 0U; slot < APP_SLOT_COUNT; slot++) {
+            app_sm_port_home_lift(&cartridges[slot], DIR_LIFTER_DOWN);
+          }
+          app_sm_port_arm_timeout(APP_SM_TIMEOUT_MOTION, 1000);
         }
-        app_sm_port_arm_timeout(APP_SM_TIMEOUT_MOTION, 1000);
-      }
-      break;
+        break;
 
-    case RELOAD_HOME_LIFTS:
-      if (event->id == APP_EV_MOTION_DONE) {
-        app_sm_port_cancel_timeout();
-        sm->reload = RELOAD_UNLOCK_DOOR;
-        app_sm_port_unlock_door();
-        app_sm_port_arm_timeout(APP_SM_TIMEOUT_UNLOCK, 1000);
-      }
-      break;
+      case RELOAD_HOME_LIFTS:
+        if (event->id == APP_EV_MOTION_DONE) {
+          app_sm_port_cancel_timeout();
+          reload->state = RELOAD_UNLOCK_DOOR;
+          app_sm_port_unlock_door();
+          app_sm_port_arm_timeout(APP_SM_TIMEOUT_UNLOCK, 1000);
+        }
+        break;
 
-    case RELOAD_UNLOCK_DOOR:
-      if (event->id == APP_EV_LOCK_RELEASED) {
-        app_sm_port_cancel_timeout();
-        sm->reload = RELOAD_WAIT_OPEN;
-        app_sm_port_arm_timeout(APP_SM_TIMEOUT_UNLOCK, 1000);
-      }
-      break;
+      case RELOAD_UNLOCK_DOOR:
+        if (event->id == APP_EV_LOCK_RELEASED) {
+          app_sm_port_cancel_timeout();
+          reload->state = RELOAD_WAIT_OPEN;
+          app_sm_port_arm_timeout(APP_SM_TIMEOUT_UNLOCK, 1000);
+        }
+        break;
 
-    case RELOAD_WAIT_OPEN:
-      if (event->id == APP_EV_DOOR_OPENED) {
-        app_sm_port_cancel_timeout();
-        sm->reload = RELOAD_WAIT_CLOSE;
-        app_sm_port_arm_timeout(APP_SM_TIMEOUT_UNLOCK, 1000);
-      }
-      break;
+      case RELOAD_WAIT_OPEN:
+        if (event->id == APP_EV_DOOR_OPENED) {
+          app_sm_port_cancel_timeout();
+          reload->state = RELOAD_WAIT_CLOSE;
+          app_sm_port_arm_timeout(APP_SM_TIMEOUT_UNLOCK, 1000);
+        }
+        break;
 
-    case RELOAD_WAIT_CLOSE:
-      if (event->id == APP_EV_DOOR_CLOSED) {
-        app_sm_port_cancel_timeout();
-        sm->reload = RELOAD_LOCK_DOOR;
-        app_sm_port_lock_door();
-        app_sm_port_arm_timeout(APP_SM_TIMEOUT_LOCK, 1000);
-      }
-      break;
+      case RELOAD_WAIT_CLOSE:
+        if (event->id == APP_EV_DOOR_CLOSED) {
+          app_sm_port_cancel_timeout();
+          reload->state = RELOAD_LOCK_DOOR;
+          app_sm_port_lock_door();
+          app_sm_port_arm_timeout(APP_SM_TIMEOUT_LOCK, 1000);
+        }
+        break;
 
-    case RELOAD_LOCK_DOOR:
-      if (event->id == APP_EV_LOCK_CONFIRMED) {
-        app_sm_port_cancel_timeout();
-        sm->reload = RELOAD_COUNT_CARTRIDGES;
-        // app_sm_port_count_cartridges();
-        app_sm_port_arm_timeout(APP_SM_TIMEOUT_MOTION, 1000);
-      }
-      break;
+      case RELOAD_LOCK_DOOR:
+        if (event->id == APP_EV_LOCK_CONFIRMED) {
+          app_sm_port_cancel_timeout();
+          reload->state = RELOAD_COUNT_CARTRIDGES;
+          // app_sm_port_count_cartridges();
+          app_sm_port_arm_timeout(APP_SM_TIMEOUT_MOTION, 1000);
+        }
+        break;
 
-    case RELOAD_COUNT_CARTRIDGES:
-      if (event->id == APP_EV_COUNT_DONE) {
-        app_sm_port_cancel_timeout();
-        sm->reload = RELOAD_COMPLETE;
-      }
-      break;
+      case RELOAD_COUNT_CARTRIDGES:
+        if (event->id == APP_EV_COUNT_DONE) {
+          app_sm_port_cancel_timeout();
+          reload->state = RELOAD_COMPLETE;
+          result.status = RELOAD_STATUS_DONE;
+        }
+        break;
 
-    case RELOAD_COMPLETE:
-    case RELOAD_FAILED:
-    default:
-      break;
+      case RELOAD_COMPLETE:
+        result.status = RELOAD_STATUS_DONE;
+        break;
+
+      case RELOAD_FAILED:
+      default:
+        result.status = RELOAD_STATUS_FAILED;
+        break;
+    }
   }
+
+  return result;
 }
 
-/*******************************************************************************
- * Private Function Definitions
- *******************************************************************************/
+void reload_sm_abort(reload_sm_t* reload) {
+  if (reload != NULL) {
+    app_sm_port_cancel_timeout();
+    reload->state = RELOAD_FAILED;
+    reload->lift_homing_end_time_ms = 0U;
+    reload->lift_homing_timeout_ms = 0U;
+  }
+}
---
+++ stepper_144pin_cmake/app/reload_sm.h
@@ -0,0 +1,74 @@
+/**
+ * @file reload_sm.h
+ * @author Chris Owens (cowens@eemn.io)
+ * @brief
+ * @version 0.1
+ * @date 2026-08-24
+ *
+ * @copyright Copyright (c) 2026 Embedded Design Solutions, LLC.  All Rights Reserved.
+ *
+ */
+
+#ifndef RELOAD_SM_H_
+#define RELOAD_SM_H_
+#include <stdint.h>
+#include "autoloader_types.h"
+#include "cartridge.h"
+/*******************************************************************************
+ * Includes
+ *******************************************************************************/
+typedef struct {
+  uint32_t status;
+  uint32_t fault_code;
+} reload_result_t;
+
+typedef enum {
+  RELOAD_STATUS_OK = 0,
+  RELOAD_STATUS_IN_PROGRESS = 1,
+  RELOAD_STATUS_DONE = 2,  // success, can transition to ready
+  RELOAD_STATUS_FAILED = 3,
+} reload_status_enum;
+
+typedef struct {
+  reload_state_enum state;
+  uint32_t lift_homing_end_time_ms;
+  uint32_t lift_homing_timeout_ms;
+} reload_sm_t;
+/*******************************************************************************
+ * Module Macros
+ *******************************************************************************/
+
+/*******************************************************************************
+ * Module Typedefs
+ *******************************************************************************/
+
+/*******************************************************************************
+ * Module Variable Definitions
+ *******************************************************************************/
+
+/*******************************************************************************
+ *Function Prototypes
+ *******************************************************************************/
+
+/**
+ * @brief Enter the reload sequence at its first step.
+ * @param sm Application state model.
+ * @param cartridges Array of cartridges.
+ */
+void reload_sm_start(reload_sm_t* sm, cartridge_t cartridges[APP_SLOT_COUNT]);
+
+/**
+ * @brief Dispatch one event to the reload sequence.
+ * @param reload Application state model.
+ * @param cartridges Array of cartridges.
+ * @param event Event to process.
+ * @return Result of the reload sm
+ */
+reload_result_t reload_sm_dispatch(reload_sm_t* reload, cartridge_t cartridges[APP_SLOT_COUNT], const app_event_t* event);
+
+/**
+ * @brief Abort the reload sequence.
+ * @param sm Application state model.
+ */
+void reload_sm_abort(reload_sm_t* reload);
+#endif /* RELOAD_SM_H_ */
